c788f4a05b6edb55374d044a5bf646eba7157910
Andy Deese <james.deese@gmail.com>
2018-02-13 18:03:37 -0500

    added completed StringPlay and HelloWorld

---
+++ src/main/java/completed/HelloWorld.java
@@ -0,0 +1,12 @@
+package completed;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int y = 5;
+		int z = 2;
+		double x = (double)y/z;
+		System.out.println(x);
+	}
+
+}
---
+++ src/main/java/completed/StringPlay.java
@@ -0,0 +1,41 @@
+package completed;
+
+// **************************************************
+// StringPlay.java
+//
+// Play with String objects
+// **************************************************
+public class StringPlay {
+
+	public static void main(String[] args) {
+		String college = new String("USC Upstate College");
+		// (a) declare the variable town as a reference to a String
+		// object and initialize it to "Anytown, USA".
+		String town = "Anytown, USA";
+
+		// (b) write an assignment statement that invokes the length
+		// method of the string class to find the length of the college
+		// String object and assigns the result to the stringLength variable
+		int stringLength = college.length();
+
+		String change1, change2, change3;
+		// (c) complete the assignment statement so that change1
+		// contains the same characters as college but all in upper case
+		change1 = college.toUpperCase();
+
+		// (d) complete the assignment statement so that change2 is the same
+		// as change1 except all capital O's are replaced with the asterisk (*)
+		// character.
+		change2 = change1.replace('O', '*');
+
+		// (e) complete the assignment statement so that change3 is the
+		// concatenation of college and town (use the concat method of the
+		// String class rather than the + operator)
+		change3 = college.concat(town);
+
+		System.out.println("stringLength:" + stringLength);
+		System.out.println("change1:" + change1);
+		System.out.println("change2:" + change2);
+		System.out.println("change3:" + change3);
+	}
+}
