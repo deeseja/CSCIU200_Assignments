854ec3cc2174cb993a1c28bbfd629355534065f1
Andy Deese <james.deese@gmail.com>
2018-02-20 20:18:14 -0500

    adding EmployeeDriver, incomplete

---
+++ src/main/java/exercises/EmployeeDriver.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.text.NumberFormat;
+
+public class EmployeeDriver {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		
+		employee.setFirstName("Fred");
+		employee.setLastName("Smith");
+		employee.setSalary(50000);
+		
+		// should print out the following:
+		// Fred Smith
+		// salary: $50,000.00
+		System.out.println(employee);
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		System.out.println("salary:" + formatter.format(employee.getSalary()));
+	}
+
+}
