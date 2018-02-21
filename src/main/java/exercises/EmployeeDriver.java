d084e11b01e03777b8b85eb5cf6f283f892ffab5
Andy Deese <james.deese@gmail.com>
2018-02-20 20:28:02 -0500

    adding EmployeeDriver constructor variable requirement

--- src/main/java/exercises/EmployeeDriver.java
+++ src/main/java/exercises/EmployeeDriver.java
@@ -7,17 +7,23 @@ public class EmployeeDriver {
 	public static void main(String[] args) {
 		// Create the Employee class to support the 
 		// methods used below.
-		Employee employee = new Employee();
+		Employee employee = new Employee("Tanya", "West", 60000);
 		
+		// should print out the following:
+		// Tanya West
+		// salary:$60,000.00
+		System.out.println(employee);
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		System.out.println("salary:" + formatter.format(employee.getSalary()) + "\n");
+
 		employee.setFirstName("Fred");
 		employee.setLastName("Smith");
 		employee.setSalary(50000);
 		
 		// should print out the following:
 		// Fred Smith
-		// salary: $50,000.00
+		// salary:$50,000.00
 		System.out.println(employee);
-		NumberFormat formatter = NumberFormat.getCurrencyInstance();
 		System.out.println("salary:" + formatter.format(employee.getSalary()));
 	}
 
